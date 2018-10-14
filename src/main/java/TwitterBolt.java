d6d2e9a9622a260f4e19d2ec3dd1b54f0235c247
Rakesh Bhavsar <rakesh.r.bhavsar@gmail.com>
2018-10-14 09:57:04 -0400

    added logic to write tweets to .csv file

--- src/main/java/TwitterBolt.java
+++ src/main/java/TwitterBolt.java
@@ -1,3 +1,4 @@
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
@@ -6,12 +7,34 @@ import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 import twitter4j.Status;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwitterBolt extends BaseBasicBolt {
+
+    private PrintWriter writer;
+    Map<Integer,String> map;
+    Integer i;
+
+    public void prepare(Map stormConf, TopologyContext context) {
+
+        String fileName = "output"+"-"+context.getThisTaskId()+"-"+context.getThisComponentId()+".csv";
+        try{
+            this.writer = new PrintWriter(stormConf.get("dirToWrite").toString()+fileName, "UTF-8");
+            this.map = new HashMap<Integer, String>();
+            this.i = 0;
+        }
+        catch (Exception e){}
+    }
 
     public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
 
+
         Status status = (Status) tuple.getValueByField("tweet");
         String tweetText = status.getText();
+        map.put(i++,tweetText);
+
 
         basicOutputCollector.emit(new Values(tweetText));
     }
@@ -20,5 +43,13 @@ public class TwitterBolt extends BaseBasicBolt {
 
         outputFieldsDeclarer.declare(new Fields("tweet"));
 
+    }
+
+    public void cleanup() {
+
+        for(String tweet:map.values()){
+            writer.println(tweet);
+        }
+        writer.close();
     }
 }
