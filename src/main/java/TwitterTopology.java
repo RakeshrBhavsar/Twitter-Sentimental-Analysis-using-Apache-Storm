5ee918efb44730c6fb601d66c3650e3fc4442e1b
Rakesh Bhavsar <rakesh.r.bhavsar@gmail.com>
2018-10-14 09:57:57 -0400

    increasing the number of bolts

--- src/main/java/TwitterTopology.java
+++ src/main/java/TwitterTopology.java
@@ -7,14 +7,16 @@ public class TwitterTopology {
 
         TopologyBuilder topologyBuilder = new TopologyBuilder();
         topologyBuilder.setSpout("twitter-data-collector", new TwitterSpout());
-        topologyBuilder.setBolt("twitter-data-extractor", new TwitterBolt())
+        topologyBuilder.setBolt("twitter-data-extractor", new TwitterBolt(),3)
                 .shuffleGrouping("twitter-data-collector");
 
         LocalCluster cluster = new LocalCluster();
         Config conf = new Config();
         conf.setDebug(true);
+        conf.put("dirToWrite", "/Users/rakeshbhavsar/Twitter Sentimental Analysis using Apache-Storm/src");
+
         cluster.submitTopology("twitter-topology", conf, topologyBuilder.createTopology());
-        Thread.sleep(10000);
+        Thread.sleep(50000);
         cluster.shutdown();
 
     }
